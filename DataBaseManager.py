1c738e776b37c1f7001d8a4704d694f2cf7865c4
TCMVince <vincent.segonne@gmail.com>
2016-12-06 12:53:02 +0100

    -m

---
+++ DataBaseManager.py
@@ -0,0 +1,216 @@
+# -*- coding: utf-8 -*-
+
+import codecs
+import sqlite3 as sq
+import sys
+
+
+
+def load_recipes_from_textFile(recipes_file):
+
+    """ Load recipes from a text file"""
+
+    database = open(recipes_file)
+    #line = database.readline().decode("utf-8")
+    line = database.readline()
+    recipes = []
+    recipe = {}
+
+    recipes_name = set([]) #To avoid duplicates
+
+    while line:
+
+        if line == "\n":
+            if recipe["type"] == "Plat principal" and recipe["recipe_name"] not in recipes_name:
+                recipes_name.add(recipe["recipe_name"])
+                recipes.append(recipe)
+                recipe = {}
+
+        else:
+            line = line.rstrip()
+            line = line.split('\t')
+
+            recipe[line[0]] = line[1]
+
+        #line = database.readline().decode("UTF-8")
+        line = database.readline()
+    database.close()
+    return recipes
+
+
+
+
+def create_recipes_database_from_textFile(recipes_file):
+
+    recipes = load_recipes_from_textFile(recipes_file)
+
+    conn = sq.connect("Recipes.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+                CREATE TABLE IF NOT EXISTS Recipes(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   url TEXT,
+                   recipe_name TEXT,
+                   type TEXT,
+                   difficulty TEXT,
+                   cost TEXT,
+                   guests_number TEXT,
+                   preparation_time TEXT,
+                   cook_time TEXT,
+                   ingredients TEXT,
+                   instructions TEXT
+                   )
+                   """
+                   )
+
+
+    for recipe in recipes:
+        cursor.execute("""
+                       INSERT INTO Recipes(url, recipe_name, type, difficulty, cost, guests_number, preparation_time, \
+                       cook_time, ingredients, instructions) VALUES(:url, :recipe_name, :type, :difficulty, :cost, :guests_number, \
+                      :preparation_time, :cook_time, :ingredients, :instructions)\
+                       """, recipe
+                        )
+
+
+
+    conn.commit()
+    conn.close()
+
+def clean_database(database_file, table):
+
+    """ Deletes duplicates in table """
+
+    conn = sq.connect(database_file)
+    cursor = conn.cursor()
+
+    command = """ delete from """+table+""" where id not in (select  min(id) from """+table+""" group by recipe_name )"""
+    cursor.execute(command)
+
+    conn.commit()
+    conn.close()
+
+def load_recipes_from_database(database_file):
+
+    conn = sq.connect(database_file)
+    cursor = conn.cursor()
+
+    col_names = ["url","recipe_name","type","difficulty","cost","guest_number", "preparation_time", "cook_time","ingredients","instructions" ]
+    recipes = []
+
+    for row in cursor.execute("SELECT * FROM Recipes"):
+        recipe = {}
+        for i, col in enumerate(row):
+            if i > 0:
+                recipe[col_names[i-1]] = col
+        recipes.append(recipe)
+    return recipes
+
+
+
+# PROFILES MANAGEMENT
+
+def exists_profile(profile_name):
+    """ Check if a a table with the profile name exists in Profile.db
+        Args :
+                - profile_name : name of the profile : str
+        Return:
+                - Bool
+    """
+
+
+    conn = sq.connect("Profile.db")
+
+    cursor = conn.cursor()
+    exists = "SELECT * FROM sqlite_master WHERE type='table' and name='"+profile_name+"'"
+    if cursor.execute(exists).fetchone():
+       conn.close()
+       return True
+    else:
+        conn.close()
+        return False
+
+def create_profile_table(profile_name):
+    """ Create a table with the profile name
+        Args :
+                - profil_name : name of the profile
+        Return : _
+    """
+    conn = sq.connect("Profile.db")
+    cursor = conn.cursor()
+    create = "CREATE TABLE " + profile_name + """(
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              opinion TEXT,
+              url TEXT,
+              recipe_name TEXT,
+              type TEXT,
+              difficulty TEXT,
+              cost TEXT,
+              guests_number TEXT,
+              preparation_time TEXT,
+              cook_time TEXT,
+              ingredients TEXT,
+              instructions TEXT
+              )
+              """
+    cursor.execute(create)
+    conn.commit()
+    conn.close()
+
+def load_liked_recipes_from_profile(profile_name):
+    """ Load the recipes that have been liked by the profile user
+        Args:
+                - profile_name : the profile name : str
+        Return:
+                - Recipe (obj)
+    """
+    conn = sq.connect("Profile.db")
+    cursor = conn.cursor()
+    loading = "SELECT url, recipe_name, type, difficulty, cost, guests_number,\
+        preparation_time, cook_time, ingredients, instructions FROM " + profile_name + \
+        " WHERE opinion='like'"
+
+    liked_recipes = []
+    col_names = ["url","recipe_name", "type", "difficulty","cost", "guests_number",\
+           "preparation_time", "cook_time", "ingredients", "instructions"]
+    for row in cursor.execute(loading):
+        #TODO CHANGER dict pour obj
+        recipe = {}
+        for i, col in enumerate(row):
+            recipe[col_names[i]] = col
+        liked_recipes.append(recipe)
+
+    conn.close()
+    return liked_recipes
+
+def load_disliked_recipes_from_profile(profile_name):
+    """ Load the recipes that have been liked by the profile user
+        Args:
+                - profile_name : the profile name : str
+        Return:
+                - Recipe (obj)
+    """
+    conn = sq.connect("Profile.db")
+    cursor = conn.cursor()
+    loading = "SELECT url, recipe_name, type, difficulty, cost, guests_number,\
+        preparation_time, cook_time, ingredients, instructions FROM " + profile_name + \
+        " WHERE opinion='dislike'"
+
+    disliked_recipes = []
+    col_names = ["url","recipe_name", "type", "difficulty","cost", "guests_number",\
+           "preparation_time", "cook_time", "ingredients", "instructions"]
+    for row in cursor.execute(loading):
+        #TODO CHANGER dict pour obj
+        recipe = {}
+        for i, col in enumerate(row):
+            recipe[col_names[i]] = col
+        disliked_recipes.append(recipe)
+
+    conn.close()
+    return disliked_recipes
+
+
+
+if __name__ == "__main__":
+    pass
+
--- main.py
+++ main.py
@@ -7,49 +7,130 @@ import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.metrics.pairwise import cosine_similarity
 import operator
+import random
+from ProfileManager import *
 
-#create_recipes_database_from_textFile("output_recipes_0312.txt")
-clean_database("Recipes.db", "Recipes")
+
+
+#create_recipes_database_from_textFile("output_recipes_0512.txt")
+#clean_database("Recipes.db", "Recipes")
+#recipes = load_recipes_from_database("Recipes.db")
+
+#vectorized_recipes = list(vectorize_recipes2(recipes).toarray())
+
+#random.shuffle(recipes)
+
+#liked_recipes =[]
+
+#recipes_to_pop = []
+#liked_count = 0
+
+# PREMIER TEST
+#for i, recipe in enumerate(recipes):
+#    if recipe["type"] == "Plat principal":
+#        print("\n")
+#        print("Recette :",recipe["recipe_name"])
+#        print("Ingrédients :",recipe["ingredients"])
+#        print("Temps de préparation :",recipe["preparation_time"])
+#        print("Temps de cuisson :",recipe["cook_time"])
+#        res = input("Je valide ?\n")
+#        if res == "y":
+#            liked_count += 1
+#            liked_recipes.append(vectorized_recipes[i])
+
+ #           recipes_to_pop.append(i)
+ #           vectorized_recipes.pop(i)
+ #           recipes.pop(i)
+#
+#    print("Recipe numéro :",i)
+#    print("Plats likés :", liked_count)
+
+ #   if len(liked_recipes) == 20:
+ #       break
+
+#kmeans = KMeans(n_clusters=4).fit(liked_recipes)
+#centers =[x for x in kmeans.cluster_centers_]
+
+
+#unranked_cos_sim = []
+#for i, recipe in enumerate(vectorized_recipes):
+#    matrix = [recipe] + centers
+#    cos_matrix = cosine_similarity(matrix)
+#    score_best_sim = max(cos_matrix[0][1:])
+#    unranked_cos_sim.append((i,score_best_sim))
+
+#sorted_recipe = sorted(unranked_cos_sim,key=operator.itemgetter(1), reverse=True)
+#print("=================================")
+#print("\t propositions\n")
+
+#for i in range(7):
+#    print(recipes[sorted_recipe[i][0]]["recipe_name"],"\n")
+
+#exists_profile("Recipes")
+#create_profile_table("Vincent")
+#liked_recipes = load_liked_recipes_from_profile("Vincent")
+#print(liked_recipes)
+
+print("\t=========================================")
+print("\n\t\tBienvenu dans EatAWeek")
+print("\n\t=========================================\n")
+
+# Chargement et vectorization des recettes
 recipes = load_recipes_from_database("Recipes.db")
-print(len(recipes))
-vectorized_recipes = list(vectorize_recipes2(recipes).toarray())
-print(type(vectorized_recipes))
+v_recipes = vectorize_recipes2(recipes).toarray()
 
-liked_recipes =[]
 
-recipes_to_pop = []
+profile_name = input("Entrez votre nom : ")
 
-for i, recipe in enumerate(recipes):
-    if recipe["type"] == "Plat principal":
-        print("\n",recipe["recipe_name"])
-        print(recipe["ingredients"])
-        print(recipe["preparation_time"])
-        print(recipe["cook_time"])
-        res = input("Je valide ?\n")
+# Si le profil existe déjà
+if exists_profile(profile_name):
+    print("\nUn profil correspond à votre nom ("+profile_name+")")
+    print("Chargement de vos données ..")
+    liked_recipes = load_liked_recipes_from_profile(profile_name)
+    disliked_recipes = load_disliked_recipes_from_profile(profile_name)
+
+    v_liked_recipes = vectorize_recipes2(liked_recipes)
+    kmeans = KMeans(nb_cluster=10).fit(v_liked_recipes)
+    centers =[x for x in kmeans.cluster_centers_]
+else:
+    print("Création de votre profil dans la base de donnée..\n")
+    res = input("Appuyez sur n'importe quelle autre touche pour continuer")
+    print("\nNous allons apprendre à vous connaitre! ")
+    print("Pour cela nous allons vous présenter des recettes aléatoirement")
+    print("Validez celles qui vous plaisent en appuyant sur la touche 'y', sur n'importe quelle autre touche sinon.")
+    print("Lorsque vous aurez selectionné 20 recettes, nous vous ferons des recommandations!\n")
+    res = input("Vous êtes prêt ? Appuyez sur n'importe quelle touche")
+    print("\n\n========== CHOIX DES RECETTES ====================")
+
+    liked_recipes = []
+    liked_recipes_name = []
+    v_liked_recipes = []
+    liked_count = 0
+    for i, recipe in enumerate(recipes):
+      print("Vous avez aimé : " + str(liked_count) + " recettes")
+      if recipe["type"] == "Plat principal":
+        print("\n")
+        print("Recette :",recipe["recipe_name"])
+        print("Ingrédients :",recipe["ingredients"])
+        print("Temps de préparation :",recipe["preparation_time"])
+        print("Temps de cuisson :",recipe["cook_time"])
+        res = input("Cette recette vous plaît (y) ?\n")
         if res == "y":
-            liked_recipes.append(vectorized_recipes[i])
+            liked_count += 1
+            liked_recipes.append(recipe)
+            liked_recipes_name.append(recipe["recipe_name"])
+            v_liked_recipes.append(v_recipes[i])
 
-            recipes_to_pop.append(i)
-            vectorized_recipes.pop(i)
-            recipes.pop(i)
-            print("LEn RECIPES",len(recipes))
-            print("LEn VECTOR RECIPES",len(vectorized_recipes))
+        if len(liked_recipes) == 5:
 
-    if len(liked_recipes) == 20:
-        break
+            kmeans = KMeans(n_clusters=5).fit(v_liked_recipes)
+            centers =[x for x in kmeans.cluster_centers_]
+            unranked_cos_sim = []
 
-kmeans = KMeans(n_clusters=4).fit(liked_recipes)
-centers =[x for x in kmeans.cluster_centers_]
-
-
-unranked_cos_sim = []
-for i, recipe in enumerate(vectorized_recipes):
-    matrix = [recipe] + centers
-    cos_matrix = cosine_similarity(matrix)
-    score_best_sim = max(cos_matrix[0][1:])
-    unranked_cos_sim.append((i,score_best_sim))
-
-print("HEYYY")
-sorted_recipe = sorted(unranked_cos_sim,key=operator.itemgetter(1), reverse=True)
-for i in range(7):
-    print(recipes[sorted_recipe[i][0]]["recipe_name"])
+            for i, recipe in enumerate(recipes):
+                if recipe["recipe_name"] not in liked_recipes_name:
+                    matrix = centers + v_recipes[i]
+                    cos_matrix = cosine_similarity(matrix)
+                    score_best_sim = max(cos_matrix[0][1:])
+                    unranked_cos_sim.append((i,score_best_sim))
+            sys.exit()
