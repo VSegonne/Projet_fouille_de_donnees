739fee438e72fae9ef63bcb7621e6512e1a0b503
ZA0D2 <zhaode0728@gmail.com>
2016-12-13 07:22:28 +0100

    add recipes

--- Programme/recipes_ver_2.php
+++ Programme/recipes_ver_2.php
@@ -5,48 +5,141 @@ include_once('vendor/simplehtmldom_1_5/simple_html_dom.php');
 
 use sylouuu\MarmitonCrawler\Recipe\Recipe;
 
-# $html = http://www.marmiton.org/recettes/recettes-index.aspx
-#function fetch_letters ($html) {
-#	$html = file_get_html($html);
-#	$rs = $html->find('div');
-#}
+function cost ($html) {
+	$html = file_get_html($html);
+	$r = $html->find('div.m_content_recette_breadcrumb');
+	return explode(" - ",trim($r[0]->plaintext))[1]."\n";
+}
 
+function fetch_letters ($html) {
+	/*
+		input : "html = http://www.marmiton.org/recettes/recettes-index.aspx"
+		output : ["http://www.marmiton.org/recettes/recettes-index.aspx?letter=A",
+		          "http://www.marmiton.org/recettes/recettes-index.aspx?letter=B",
+		          ...]
+	*/
+	$queries = array();
+	$html = file_get_html($html);
+	$rs = $html->find('div.m-lst-page ul li a');
+	foreach ($rs as $r) {
+		array_push($queries,"http://www.marmiton.org".$r->href);
+	}
+	return $queries;
+}
 
 function fetch_ingredients ($html) {
-  $html = file_get_html($html);
-  $ret = $html->find('script[type="text/javascript"]');
-  foreach ($ret as $r) {
-    if (substr($r,0,46) == "<script type=\"text/javascript\">var m_dataLayer") {
-      $r = preg_replace('/\s+/', '', $r);
-      return explode(",",explode("\",\"recipeType\"",explode("\"contentInfo\":{\"recipeIngredients\":\"",explode(';',$r)[0])[1])[0]);
-    }
-  }
+	/*
+		input : "http://www.marmiton.org/recettes/recettes-index.aspx?letter=A"
+		output : ["http://www.marmiton.org/recettes/recettes-index.aspx?ingredient=abadeche",
+                  "http://www.marmiton.org/recettes/recettes-index.aspx?ingredient=abondance",
+                  ...]
+	*/
+	$ingres = array();
+	$html = file_get_html($html);
+	$rs = $html->find('div.content ul.m-lsting-ing li a');
+	foreach ($rs as $r) {
+		array_push($ingres,"http://www.marmiton.org".$r->href);
+	}
+	return $ingres;
+}
+
+function fetch_pages ($html) {
+	/*
+		input : "http://www.marmiton.org/recettes/recettes-index.aspx?ingredient=abadeche"
+		output : []
+		inpout : "http://www.marmiton.org/recettes/recettes-index.aspx?ingredient=lait"
+		output : ["http://www.marmiton.org/recettes/recettes-index.aspx?ingredient=lait&page=1",
+                  "http://www.marmiton.org/recettes/recettes-index.aspx?ingredient=lait&page=2",
+                  ...]
+	*/
+	$pages = array();
+	$html = file_get_html($html);
+	$rs = $html->find('div.m_bloc_cadre div.content div.ToCPagingContainer a');
+	foreach ($rs as $r) {
+		array_push($pages,"http://www.marmiton.org".$r->href);
+	}
+	return $pages;
 }
 
 function fetch_recipes ($html) {
+	/*
+		fonction qui une liste d'urls (/recettes/recette_)
+		niveau = une page des résultats d'une requete
+	*/
 	$recipes = array();
 	$html = file_get_html($html);
-	$rs = $html->find('div[class="content"] ul.m-lsting-recipe');
+	$rs = $html->find('div.content ul.m-lsting-recipe li a');
 	foreach ($rs as $r) {
-		$r = $r->find('li a');
-		foreach ($r as $a) {
-			array_push($recipes,$a->href);
-		}
+		array_push($recipes,"http://www.marmiton.org".$r->href);
 	}
 	return $recipes;
 }
 
-function write_recipe($page) {
+function fetch_all ($file,$html) {
 
-	
+	$bd = array();
 
-	echo $page."\n";
+	while (count($bd) <= 10000) {
 
-	$recipe = new Recipe($page);
+		foreach (fetch_letters($html) as $letter) {
+			foreach (fetch_ingredients($letter) as $ingredient) {
+				echo "page ingre : ".$ingredient."\n";
+				if (count(fetch_pages($ingredient)) == 0) {
+					foreach (fetch_recipes($ingredient) as $recipe) {
+						if (!in_array($recipe,$bd) and substr($recipe,0,41) == "http://www.marmiton.org/recettes/recette_") {
+							array_push($bd,$recipe);
+							#echo $recipe."\n";
+							write_recipe($file,$recipe);
+						}
+					}
+				} else {
+					echo "N-pages"."\n";
+					foreach (fetch_pages($ingredient) as $page) {
+						foreach (fetch_recipes($page) as $recipe) {
+							if (!in_array($recipe,$bd) and substr($recipe,0,41) == "http://www.marmiton.org/recettes/recette_") {
+								array_push($bd,$recipe);
+								#echo $recipe."\n";
+								write_recipe($file,$recipe);
+							}
+						}
+					}
+				}
+				echo "#recipes = ".count($bd)."\n";
+			}
+		}
+	}
+}
+
+# ==================
+# === SEPARATOR ====
+# ==================
+
+function write_ingredients ($html) {
+	/*
+		fonction utilisée dans l'écriture d'une recette
+		forme d'écriture d'ingredients du package Marmitoncrawler est rejetée
+		fonction qui retourne une liste d'ingredients
+		niveau = page d'une recette
+	*/
+	$html = file_get_html($html);
+	$ret = $html->find('script[type="text/javascript"]');
+	foreach ($ret as $r) {
+		if (substr($r,0,46) == "<script type=\"text/javascript\">var m_dataLayer") {
+			$r = preg_replace('/\s+/', '', $r);
+			return explode(",",explode("\",\"recipeType\"",explode("\"contentInfo\":{\"recipeIngredients\":\"",explode(';',$r)[0])[1])[0]);
+		}
+	}
+}
+
+function write_recipe($file,$page) {
+
+	$recipe = new Recipe($page); # classe Recipe du package Marmitoncrawler
 	$recipe = $recipe -> getRecipe();
 
-	if (count($recipe)!= 0) {
-		$file = fopen('recipes_index_A.txt', 'a');
+	if (count($recipe)!= 0 and $recipe["type"] == "Plat principal") {
+
+		echo $page."\n";
+
 		fwrite($file,"url\t");
 		fwrite($file, $page."\n");
 		fwrite($file,"recipe_name\t");
@@ -64,7 +157,7 @@ function write_recipe($page) {
 		fwrite($file,"cook_time\t");
 		fwrite($file, $recipe["cook_time"]."\n");
 		fwrite($file, "ingredients\t");
-		$a = fetch_ingredients($page);
+		$a = write_ingredients($page);
 		for ($i=0; $i<count($a)-1; $i++) { 
 			fwrite($file, $a[$i]."|");
 		}
@@ -75,46 +168,15 @@ function write_recipe($page) {
 			fwrite($file,str_replace("?","'", trim($s))." ");
 		}
 		fwrite($file, "\n\n");
-		fclose($file);
 	}
 }
 
-# ======
-#  MAIN
-# ======
+# ============
+# === MAIN ===
+# ============
 
-
-$crawled_recipes = array();
-
-$list_ing = array('abadeche','abondance','abricot','absinthe','achatine','acide-citrique','acide-lactique','acide-tartrique','acidifiant','acoupa','adjwain','after-eight','agar-agar','agneau','agrume-confit','aiguillette-de-canard','aiguillette-de-poulet','aiguillettes-de-dinde','ail','ail-semoule','aile-de-poulet','aillet','aioli','airelles','alcool-de-poire','alevin','alfafa','algue-dulse','algue-japonaise','algue-sechee','alkermes','all-bran','alose','alouette','amande','amarante','amaretti','amaretto','amidon','ananas','anchoiade','anchois','andouille','andouillette','aneth','angelique','angostura','anguille','anis','anis-en-poudre','anis-etoile','anisette','anti-pectine','apericube','aperifruit','appenzeller','arachide','araignee-de-mer','arbouse','armagnac','armoise','aromate','arome','arome-amande-amere','arome-citron','arome-fleur-d-oranger','arome-maggi','arome-vanille','arquebuse','arrow-root','artichaut','artichaut-violet','asafoetida','asiago','aspartame','asperge','asperule','asti-spumante','atriau','attieke','aubergine','autruche','avocat','avoine');
-
-foreach ($list_ing as $ing) {
-	$list_search = array("http://www.marmiton.org/recettes/recettes-index.aspx?ingredient=".$ing,
-	"http://www.marmiton.org/recettes/recettes-index.aspx?ingredient=".$ing."&page=2",
-	"http://www.marmiton.org/recettes/recettes-index.aspx?ingredient=".$ing."&page=3",
-	"http://www.marmiton.org/recettes/recettes-index.aspx?ingredient=".$ing."&page=4",
-	"http://www.marmiton.org/recettes/recettes-index.aspx?ingredient=".$ing."&page=5",
-	"http://www.marmiton.org/recettes/recettes-index.aspx?ingredient=".$ing."&page=6",
-	"http://www.marmiton.org/recettes/recettes-index.aspx?ingredient=".$ing."&page=7",
-	"http://www.marmiton.org/recettes/recettes-index.aspx?ingredient=".$ing."&page=8",
-	"http://www.marmiton.org/recettes/recettes-index.aspx?ingredient=".$ing."&page=9",
-	"http://www.marmiton.org/recettes/recettes-index.aspx?ingredient=".$ing."&page=10"
-	);
-
-	foreach ($list_search as $html) {
-		foreach (fetch_recipes($html) as $page) {
-			if (substr($page,0,18) == "/recettes/recette_") {
-				$code = hash("md5",$page,false);
-				if (! in_array($code, $crawled_recipes)) {
-					write_recipe("http://www.marmiton.org".$page);
-					array_push($crawled_recipes,$code);
-				}
-			}
-		}
-	}
-}
-
-
-#write_recipe("http://www.marmiton.org/recettes/recette_tartilla-aux-abricots_313409.aspx");
+$file = fopen('output_recipes_1212.txt','a');
+fetch_all($file,"http://www.marmiton.org/recettes/recettes-index.aspx");
+fclose($file);
 
 ?>
